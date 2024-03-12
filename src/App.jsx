547e6a113f95db9df506dbad872aa558142bfd31
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-03-12 23:13:51 +0330

    Calculator section and the screen added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
 function App() {
   return (
     <>
       <section className="calculator-section">
+        <div className="screen-part">
+          <div className="first-screen-part">10 +</div>
+          <div className="second-screen-part">5</div>
+        </div>
 
 
       </section>
